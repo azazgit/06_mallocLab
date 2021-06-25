5a333f26d59fbcab2526181fee580ec65bd7b13e
azazgit <anoshzahir@hotmail.com>
2021-06-25 10:32:43 +0100

    config.h: updated TRACEDIR

--- config.h
+++ config.h
@@ -11,8 +11,9 @@
 /*
  * This is the default path where the driver will look for the
  * default tracefiles. You can override it at runtime with the -t flag.
- */
-// #define TRACEDIR "/afs/cs/project/ics2/im/labs/malloclab/traces/"
+ *
+ * #define TRACEDIR "/afs/cs/project/ics2/im/labs/malloclab/traces/"
+ */ 
 #define TRACEDIR "/home/az/Desktop/15-213/assignments/06_malloc/traceFiles/"
 /*
  * This is the list of default tracefiles in TRACEDIR that the driver
